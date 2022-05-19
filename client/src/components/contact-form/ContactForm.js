f5e53e0b39779cbfde21171dc3fd3213edb95668
emma@beinproximity.com <emma@beinproximity.com>
2022-05-19 08:35:07 -0600

    added form submissions using emailjs

--- client/src/components/contact-form/ContactForm.js
+++ client/src/components/contact-form/ContactForm.js
@@ -1,43 +1,33 @@
-// WHEN I am presented with the Contact section
-// THEN I see a contact form with fields for a name, an email address, and a message
-// WHEN I move my cursor out of one of the form fields without entering text
-// THEN I receive a notification that this field is required
-// WHEN I enter text into the email address field
-// THEN I receive a notification if I have entered an invalid email address
-
-// EmailJS - email contact submissions to myself
-
-import React, {useState} from "react";
+import React, { useRef } from 'react';
+import emailjs from 'https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js';
 
 const ContactForm = () => {
+  const form = useRef();
 
-    const [formData, setFormData] = useState({});
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    // const handleFormChange = (e) => {
-    //     setFormData({
-    //         ...formData,
-    //         [e.target.name]: e.target.name
-    //     })
-    // }
+    emailjs.sendForm('service_3xdnmyq', 'template_n5t6q1y', form.current, '-jJWADHxvYDm_MRPM')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
-    const sendContact = (e) => {
-        e.preventDefault();
-        console.log("submitting form")
-    }
-
-    return (
-        <React.Fragment id="contact-me-form">
-            <h1 id="contact-me">Slide in my DM's</h1>
-                <form className="form" style={{display: 'block'}} onSubmit={sendContact}>
-                    <input name="formName" type="text" placeholder="Full Name" value={formData.formName} />
-                    <input name="formEmail" type="email" placeholder="Email Address" value={formData.formEmail} />
-                    <input name="formMessage" type="text" placeholder="Message" />
-                    <button type="submit" onClick={sendContact}>
-                        Send it!
-                    </button>
-                </form>
-        </React.Fragment>
-    )
-}
+  return (
+    <div>
+        <h1 id="contact-me">Slide in my DM's</h1>
+      
+        <form ref={form} onSubmit={sendEmail}>
+        
+        <div><input type="text" placeholder="Name" name="user_name" /></div>
+        <div><input type="email" placeholder="Email" name="user_email" /></div>
+        <div><textarea placeholder="Message" name="message" /></div>
+        <input type="submit" value="Send it!" id="send-it" />
+        </form>
+    </div>
+  );
+};
 
 export default ContactForm;
