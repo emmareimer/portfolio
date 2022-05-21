1fee51a3a145fef68d2db8db934e60b320137e00
emma@beinproximity.com <emma@beinproximity.com>
2022-05-21 11:57:46 -0600

    changed styling, added more logos

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -4,29 +4,35 @@ import cssPic from "./css.png"
 import jsPic from "./js.png"
 import git from "./git.png"
 import node from "./node.png"
-import jquery from "./jquery.png"
 import mysql from "./mysql-logo.png"
-import bootstrapLogo from "./bootstrap.png"
 import reactLogo from "./react.png"
 import mdb from "./mdb.webp"
 import graphQL from "./graphql.webp"
+import apolloPic from "./apollo.png"
+import expressjsPic from "./expressjs.png"
+import githubLogo from "./githublogo.png"
+import jwtLogo from "./jwt.png"
+import reduxLogo from "./redux.png"
 
 const Skills = () => {
     return (
         <div id="skillDiv">
             <div><h1 id="skillsInPics">Skills</h1></div>
             <div id="skills">
-                <img src={hTmL} alt="html logo" class="skillpic" />
-                <img src={cssPic} alt="css logo" class="skillpic" />
-                <img src={jsPic} alt="javascript logo" class="skillpic" />
-                <img src={git} alt="git logo" class="skillpic" />
-                <img src={node} alt="node logo" class="skillpic" />
-                <img src={jquery} alt="jquery logo" class="skillpic" />
-                <img src={mysql} alt="mysql logo" class="skillpic" />
-                <img src={bootstrapLogo} alt="bootstrap logo" class="skillpic" />
-                <img src={reactLogo}alt="react logo" class="skillpic" />
-                <img src={mdb} alt="mongodb logo" class="skillpic" />
-                <img src={graphQL}alt="graph ql logo" class="skillpic" />
+                <img src={hTmL} alt="html logo" className="skillpic" />
+                <img src={cssPic} alt="css logo" className="skillpic" />
+                <img src={jsPic} alt="javascript logo" className="skillpic" />
+                <img src={git} alt="git logo" className="skillpic" />
+                <img src={node} alt="node logo" className="skillpic" />
+                <img src={mysql} alt="mysql logo" className="skillpic" />
+                <img src={reactLogo}alt="react logo" className="skillpic" />
+                <img src={mdb} alt="mongodb logo" className="skillpic" />
+                <img src={graphQL}alt="graph ql logo" className="skillpic" />
+                <img src={apolloPic} alt="apollo logo" className="skillpic" />
+                <img src={expressjsPic} alt="expressjs logo" className="skillpic" />
+                <img src={githubLogo} alt="github logo" className="skillpic" />
+                <img src={jwtLogo} alt="jwt logo" className="skillpic" />
+                <img src={reduxLogo} alt="redux logo" className="skillpic" />
             </div>
         </div>
     );
